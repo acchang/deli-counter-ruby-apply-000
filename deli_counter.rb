d8d356b999534c273f192c40f6a53868c6b747c8
Andrew C <acchang4@gmail.com>
2020-04-21 11:31:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,13 +14,18 @@ def line(katz_deli)
   end
 end
 
+# we need a "katz_deli = []" outside the method to establish empty
+# we need a line_positions inside because the array now has numbers
+
+katz_deli = []
+
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    number = 1
-    string = "The line is currently: "
-    katz_deli.each do |customer|
+      number = 1
+      string = "The line is currently: "
+      katz_deli.each do |customer|
       string += "#{number}. #{customer}"
       number += 1
     end
